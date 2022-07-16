95a7d822e62bc6e9c1178041d3af35b4f2106fea
AurorasChaos <38855075+AurorasChaos@users.noreply.github.com>
2022-07-16 13:58:52 +0100

    Add files via upload

---
+++ Assets/Scripts/CloneFrameHandle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class CloneFrameHandle
+{
+    List<Vector3> localPositions = new List<Vector3>();
+    List<Quaternion> localRotations = new List<Quaternion>();
+    List<FrameAction> FrameActions = new List<FrameAction>();
+    public int CurrentFrameBeingPlayed = 0;
+    public bool CanPlay = true;
+
+    public void AddNewTransformation(Transform transform, int ActionNumber)
+    {
+        localPositions.Add(transform.localPosition);
+        localRotations.Add(transform.localRotation);
+
+
+
+    }
+
+}
+
